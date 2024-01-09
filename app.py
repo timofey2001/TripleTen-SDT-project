58f494ae36c25620f7faea3e4bf84e0bbd128f0e
timofey2001 <39975417+timofey2001@users.noreply.github.com>
2024-01-08 18:07:45 -0800

    changed how select works

--- app.py
+++ app.py
@@ -30,12 +30,12 @@ car_models = cars["model"].unique().tolist()
 selected_car_model = st.selectbox("Choose a car model:", car_models)
 filtered_df = cars[cars["model"] == selected_car_model]
 price_distribution = filtered_df["price"].value_counts()
+plt.figure(figsize=(10, 6)) 
 plt.bar(price_distribution.index, price_distribution.values)
 plt.xlabel("Car price ($)")
 plt.ylabel("Number of cars")
-plt.title("Price distribution for " + selected_car_model)
-st.pyplot(plt)
-
+plt.title(f"Price Distribution for {selected_car_model}")
+st.pyplot()
 
 if st.checkbox('Vehicle price compared to odometer') == True:
     plt.figure(figsize=(10, 6))
@@ -57,6 +57,8 @@ plt.ticklabel_format(style='plain', axis='x')
 
 st.markdown("Using the data in this chart we can see that the majority of vehicles sell for less than 20 thousand dollars.")
 
+st.title('Conclusion')
+st.markdown("Our examination of the vehicle sales data yielded profound insights into the used car market's dynamics, highlighting the role of multiple factors in shaping car prices. We discovered a significant impact of a car's condition on its market value, with well-maintained cars fetching higher prices. This is especially true for newer vehicles, which tend to be pricier, and for 'salvage' vehicles, which are often more affordable. A notable observation was the preponderance of outliers, mainly cars in 'good' or 'like new' condition. This suggests a higher incidence of rare or high-end brands within these categories. Moreover, there was a distinct connection between a car's mileage and its selling price. Cars with lower mileage typically command higher prices, underscoring the importance of mileage in determining a car's worth. These insights are extremely valuable for participants in the car industry, aiding in strategic planning and enabling buyers to make more educated decisions. Nonetheless, there is still room for further investigation. Future research could include forecasting car prices and examining specific car segments in greater detail, potentially offering a more intricate understanding of the used car market.")
 if st.checkbox('Show Price Distribution by Vehicle Condition'):
     plt.figure(figsize=(10, 6))
     ax = sns.boxplot(x='condition', y='price', data=cars)
@@ -65,5 +67,4 @@ if st.checkbox('Show Price Distribution by Vehicle Condition'):
     ax.set_ylabel('Price')
     ax.set_ylim(0, 120000)
     st.pyplot(plt)
-    st.title('Conclusion')
-    st.markdown("Our examination of the vehicle sales data yielded profound insights into the used car market's dynamics, highlighting the role of multiple factors in shaping car prices. We discovered a significant impact of a car's condition on its market value, with well-maintained cars fetching higher prices. This is especially true for newer vehicles, which tend to be pricier, and for 'salvage' vehicles, which are often more affordable. A notable observation was the preponderance of outliers, mainly cars in 'good' or 'like new' condition. This suggests a higher incidence of rare or high-end brands within these categories. Moreover, there was a distinct connection between a car's mileage and its selling price. Cars with lower mileage typically command higher prices, underscoring the importance of mileage in determining a car's worth. These insights are extremely valuable for participants in the car industry, aiding in strategic planning and enabling buyers to make more educated decisions. Nonetheless, there is still room for further investigation. Future research could include forecasting car prices and examining specific car segments in greater detail, potentially offering a more intricate understanding of the used car market.")
+    
